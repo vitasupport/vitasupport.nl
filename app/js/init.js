0b87c8d3db6e7fc2b58380f0a37c5b226028a2d8
Stefan van Groningen <groninge@hotmail.com>
2017-05-19 20:31:27 +0200

    sidenav working with navbar-fixed

--- app/js/init.js
+++ app/js/init.js
@@ -2,7 +2,12 @@
   $(function(){
 
     $('.parallax').parallax();
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      }
+    );
+    $('.collapsible').collapsible();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
