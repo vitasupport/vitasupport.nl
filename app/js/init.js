2064561c9a1c35b3533331a05ed5ac8267796162
Stefan van Groningen <groninge@hotmail.com>
2017-07-27 09:29:34 +0200

    Added modal and form.

--- app/js/init.js
+++ app/js/init.js
@@ -2,6 +2,7 @@
   $(function(){
 
     $('.parallax').parallax();
+    $('.modal').modal();
     $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
