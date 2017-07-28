f7ca19d5afa28674e3cc628c59abf81dd850dda6
Stefan van Groningen <groninge@hotmail.com>
2017-07-28 20:14:34 +0200

    Added form layout and submission

--- app/js/init.js
+++ app/js/init.js
@@ -2,7 +2,18 @@
   $(function(){
 
     $('.parallax').parallax();
-    $('.modal').modal();
+    $('.modal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        inDuration: 300, // Transition in duration
+        outDuration: 200, // Transition out duration
+        startingTop: '4%', // Starting top style attribute
+        endingTop: '10%' // Ending top style attribute
+      }
+    );
+    $(document).ready(function() {
+    $('select').material_select();
+  });
     $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
