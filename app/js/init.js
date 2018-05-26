8fa58a9f5b044124e34a51b5b123fe122d713141
Stefan van Groningen <groninge@hotmail.com>
2018-05-26 19:50:15 +0200

    All options for modals are default

--- app/js/init.js
+++ app/js/init.js
@@ -1,13 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems, {
-    dismissible: true, // Modal can be dismissed by clicking outside of the modal
-    opacity: .5, // Opacity of modal background
-    inDuration: 300, // Transition in duration
-    outDuration: 200, // Transition out duration
-    startingTop: '4%', // Starting top style attribute
-    endingTop: '10%' // Ending top style attribute
-  });
+  var instances = M.Modal.init(elems);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
