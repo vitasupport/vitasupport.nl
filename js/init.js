17ffcaf341f1092dd41f75ce877fcb2190e22558
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-31 17:55:04 +0000

    Deploy vitasupport/vitasupport.nl to github.com/vitasupport/vitasupport.nl.git:gh-pages

---
+++ js/init.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
